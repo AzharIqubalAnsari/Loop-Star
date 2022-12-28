3bdeb8f13eba450ac62fd5f8e4100da0596befe1
AzharIqubalAnsari <azhar9507@gmail.com>
2022-12-28 23:15:30 +0530

    JavaBasics.java and add modified design

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,9 +1,12 @@
+import java.util.*;
 public class JavaBasics{
     public static void main(String args[]) {
+      // First Program.
       System.out.println("Hello Azhar!");
       System.out.print("Hello World!\n");
       System.out.println("1 2 3 4 5\n");
-      System.out.print("Hello World \nwelocome Azhar\n Here's your first code\n");
+      System.out.print("Hello World \n Welocome Azhar\n Here's your first code\n");
+      // Printing '*' pattern.
       System.out.println("****");
       System.out.println("***");
       System.out.println("**");
