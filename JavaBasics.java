82c59796f0e0ec03cc8e81995a3fe9174c223774
AzharIqubalAnsari <azhar9507@gmail.com>
2022-12-28 23:05:27 +0530

    Initial Commit

---
+++ JavaBasics.java
@@ -0,0 +1,13 @@
+public class JavaBasics{
+    public static void main(String args[]) {
+      System.out.println("Hello Azhar!");
+      System.out.print("Hello World!\n");
+      System.out.println("1 2 3 4 5\n");
+      System.out.print("Hello World \nwelocome Azhar\n Here's your first code\n");
+      System.out.println("****");
+      System.out.println("***");
+      System.out.println("**");
+      System.out.println("*");
+    }  
+}
+// boilerplate code
