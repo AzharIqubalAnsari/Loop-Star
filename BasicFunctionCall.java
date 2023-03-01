6d073915d32508649ef8f9b76cfd520251cd2692
AzharIqubalAnsari <azhar9507@gmail.com>
2023-03-01 23:49:12 +0530

    Print Hello using Funtion call.

---
+++ BasicFunctionCall.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class BasicFunctionCall{
+    public static int printHello(){
+        System.out.print("Hello World");
+        return 0;
+    }
+    public static void main(String args[]){
+        printHello();
+    }
+}
